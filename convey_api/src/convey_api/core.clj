920965b12fbcb51ffb49717ad675229767c18200
tundrax <sherzod@gmail.com>
2019-12-17 21:24:13 +0900

    upd

---
+++ convey_api/src/convey_api/core.clj
@@ -0,0 +1,15 @@
+(ns convey-api.core
+  (:gen-class)
+  (:require [org.httpkit.server :as http]))
+
+(defonce server (atom nil))
+
+(defn app [req]
+  (prinln (:uri req))
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "<h1>hello</h1>"})
+
+(defn -main [& args]
+  (reset! server (http/run-server app {:port 8080}))
+  (println "Server running on port 8080"))
